0ce7d4f258c89bc3c13e23007d1639ebc93a026d
apexlawnaniket <abhavsar@apexlawncompany.com>
2024-11-29 17:56:46 +0530

    fixed configuration

--- app/components/navbar/Navbar.js
+++ app/components/navbar/Navbar.js
@@ -1,13 +1,19 @@
-import React from "react";
-import styles from "./Navbar.css";
-import logo from "../../../public/assets/apex-lawn.png";
-import Image from "next/image";
+import React from 'react';
+import styles from './Navbar.css';
+//import logo from '../../../public/assets/apex-lawn.png';
+import Image from 'next/image';
+import { basePath } from '@/next.config';
 
 const Navbar = () => {
   return (
     <>
-      <nav className="container">
-        <Image src={logo} width={75} height={75} alt="Apex Logo" />
+      <nav className='container'>
+        <Image
+          src={`${basePath}/assets/apex-lawn.png`}
+          width={75}
+          height={75}
+          alt='Apex Logo'
+        />
         <ul>
           <li>Home</li>
           <li>Lawn Care</li>
@@ -15,7 +21,7 @@ const Navbar = () => {
           <li>Land Scaping </li>
           <li>More products</li>
           <li>
-            <button className="btn">Contact</button>
+            <button className='btn'>Contact</button>
           </li>
         </ul>
       </nav>
