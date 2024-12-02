f42dd29f88a8c93417ff6f293491157f1f100468
apexlawnaniket <abhavsar@apexlawncompany.com>
2024-12-02 12:21:48 +0530

    change in information section

--- app/components/Programs/Information/information.js
+++ app/components/Programs/Information/information.js
@@ -1,12 +1,21 @@
-import React from "react";
-import "./information.css";
+import React from 'react';
+import './information.css';
 
-const Information = ({ headerText, informationText }) => {
+const Information = ({ headerText }) => {
   return (
-    <div className="programContainer">
-      <div className="headerText">{headerText}</div>
+    <div className='programContainer'>
+      <div className='headerText'>{headerText}</div>
 
-      <div className="informationText">{informationText}</div>
+      <div className='informationText'>
+        <p> Eco-Utility​​​​​​​​​​​​​​ Based in Apex, NC </p> <br />
+        <p>We kicked off Apex Lawn Company March of 2019.</p>
+        <br />
+        <p>
+          Ethics count; our company cares about delivering an outstanding
+          product to our clients and providing good careers for our team. Every
+          opportunity is a step forward in building the American Dream.
+        </p>
+      </div>
     </div>
   );
 };
--- app/components/Programs/Programs.js
+++ app/components/Programs/Programs.js
@@ -1,17 +1,12 @@
-import React from "react";
-import Information from "./Information/information";
-import "./Programs.css";
+import React from 'react';
+import Information from './Information/information';
+import './Programs.css';
 
 const Programs = () => {
   return (
-    <div className="programs">
-      <div className="single-program">
-        <Information
-          headerText={"About Our Foundation"}
-          informationText={
-            "Eco-Utility​​​​​​​​​​​​​​ Based in Apex, NC We kicked off Apex Lawn Company March of 2019.Ethics count; our company cares about delivering an outstanding product to our clients and providing good careers for our team. Every opportunity is a step forward in building the American Dream."
-          }
-        />
+    <div className='programs'>
+      <div className='single-program'>
+        <Information headerText={'About Our Foundation'} />
       </div>
     </div>
   );
