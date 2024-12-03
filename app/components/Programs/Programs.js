4b3ed71beb00cea98597dfa9adec9bd057d85deb
apexlawnaniket <abhavsar@apexlawncompany.com>
2024-12-03 19:28:38 +0530

    added information css file

--- app/components/Programs/Programs.js
+++ app/components/Programs/Programs.js
@@ -1,21 +1,20 @@
-import React from "react";
-import ApexLawn from "./Information/ApexLawn/ApexLawn";
-import Competitors from "./Information/Competitors/Competitors";
-import Foundation from "./Information/Foundation/Foundation";
-import Information from "./Information/Information";
-import "./Programs.css";
+import React from 'react';
+import ApexLawn from './Information/ApexLawn/ApexLawn';
+import Competitors from './Information/Competitors/Competitors';
+import Foundation from './Information/Foundation/Foundation';
+import './Programs.css';
 
 const Programs = () => {
   return (
-    <div className="programs">
-      <div className="single-program">
-        <Foundation headerText={"About Our Foundation"} />
+    <div className='programs'>
+      <div className='single-program'>
+        <Foundation headerText={'About Our Foundation'} />
       </div>
-      <div className="single-program">
-        <Competitors headerText={"Apex Lawn Company"} />
+      <div className='single-program'>
+        <Competitors headerText={'Apex Lawn Company'} />
       </div>
-      <div className="single-program">
-        <ApexLawn headerText={"Average Competitors"} />
+      <div className='single-program'>
+        <ApexLawn headerText={'Average Competitors'} />
       </div>
     </div>
   );
