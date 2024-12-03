49b06a887f5c344711c1a8d05f41a21bf922e56c
apexlawnaniket <abhavsar@apexlawncompany.com>
2024-12-03 19:43:01 +0530

    added information css file-2

--- app/page.js
+++ app/page.js
@@ -1,15 +1,12 @@
-import Image from "next/image";
-import About from "./components/About/About";
-import Main from "./components/Main/Main";
-import Navbar from "./components/navbar/Navbar";
-import Programs from "./components/Programs/Programs";
-import Program from "./components/Programs/Programs";
-import styles from "./page.module.css";
+import About from './components/About/About';
+import Main from './components/Main/Main';
+import Navbar from './components/navbar/Navbar';
+import styles from './page.module.css';
 
 export default function Home() {
   return (
     <>
-      <div className="image-container">
+      <div className='image-container'>
         <Navbar />
         <Main />
         <About />
