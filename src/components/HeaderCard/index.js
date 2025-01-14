f2d2a949cd5e831af2e1e302d4da61590d1e09a0
aplexlawnaniket <abhavsar@apexlawncompany.com>
2025-01-14 13:03:53 +0530

    change path for image

--- src/components/HeaderCard/index.js
+++ src/components/HeaderCard/index.js
@@ -1,22 +1,22 @@
-import Image from "next/image";
-import Link from "next/link";
-import styles from "./headercard.module.css";
-import { basePath } from "@/next.config";
+import Image from 'next/image';
+import Link from 'next/link';
+import styles from './headercard.module.css';
+import { basePath } from '@/next.config';
 
 function HeaderCard({ item }) {
   const { type, text, image, path } = item || {};
   return (
     <Link href={path} className={styles.headerCard}>
-      {type === "text" ? (
+      {type === 'text' ? (
         <p className={styles.headerName}>{text}</p>
       ) : (
         <Image
-          alt="apex lawn"
-          src={`${basePath}${image}`}
+          alt='apex lawn'
+          src={`${image}`}
           width={129}
           height={84}
           style={{
-            position: "relative",
+            position: 'relative',
             // top: "-2px",
             // objectFit: "cover",
           }}
