909a11168a619b0cf3c26ce3e656a43f14143c18
aplexlawnaniket <abhavsar@apexlawncompany.com>
2025-03-03 22:17:20 +0530

    signup form complete

--- app/services/service.js
+++ app/services/service.js
@@ -14,3 +14,24 @@ export const loginService = async (email, password) => {
     throw error;
   }
 };
+
+
+
+
+  export const signUpUser = async (email, password, first_name, last_name) => {
+    const signUpUrl = process.env.NEXT_PUBLIC_SIGNUP_URL;
+  try {
+  const response = await fetch(signUpUrl, {
+    method: 'POST',
+    body: JSON.stringify({ email, password, first_name, last_name }),
+    headers: {
+      'Content-Type': 'application/json',
+    },
+  });
+  return response.json();
+} catch (error) {
+  console.error('Error in signUpUser:', error);
+  throw error;
+}
+
+} 
--- app/signuplogin/page.js
+++ app/signuplogin/page.js
@@ -3,7 +3,7 @@ import { useState } from "react";
 import styles from "./signuplogin.module.css";
 import { useRouter } from "next/navigation";
 import { signIn } from "next-auth/react";
-
+import { signUpUser } from "../services/service";
 const SignupLogin = () => {
     const [isLoginScreen, setIsLoginScreen] = useState(false);
     const [error, setError] = useState(null);
@@ -19,8 +19,15 @@ const SignupLogin = () => {
     }
 
     const handleSubmit = async (e) => {
+      debugger
+      console.log(formData);
+      console.log(e);
+
+
+
         e.preventDefault();
-        try {
+        if(isLoginScreen){
+          try {
             const result = await signIn("credentials", {
                 email: formData.email,
                 password: formData.password,
@@ -36,8 +43,39 @@ const SignupLogin = () => {
             }
         } catch (error) {
             setError(error.message);
-            setIsLoggedIn(false);
+            setIsLoginScreen(false);
         }
+
+      }else{
+        try {
+          const result = await signUpUser(formData.email, formData.password, formData.first_name, formData.last_name);
+          if(result.status === 200 && result.message === "User created successfully"){
+           alert("User created successfully");
+            setError(null);
+            setIsLoginScreen(true);
+
+            setFormData({
+              email: "",
+              password: "",
+              first_name: "",
+              last_name: "",
+            });
+          } else if(result.status === 500 && result.error.includes('duplicate')){
+            alert("User email already exists");
+            setError(result.error);
+            setIsLoginScreen(false);
+            setFormData({
+              email: "",
+              password: "",
+              first_name: "",
+              last_name: "",
+            });
+          }
+        } catch (error) {
+          setError(error.message);
+        }
+      }
+        
     }
     return (
        <>
