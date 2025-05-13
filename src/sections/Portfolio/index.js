2035158083da9ae76ad0eaa8fe8d4e38fd816cc7
apexlawndivya <djanga@apexlawncompany.com>
2025-05-13 14:17:09 +0530

    portfolio_instagram_icons

--- src/sections/Portfolio/index.js
+++ src/sections/Portfolio/index.js
@@ -3,6 +3,7 @@ import TransparentBtn from "@/src/components/TransparentBtn";
 import styles from "./portfolio.module.css";
 import Carousel from "@/src/components/Carousel";
 import Link from "next/link";
+import Image from "next/image";
 
 function Portfolio() {
   return (
@@ -23,14 +24,49 @@ function Portfolio() {
         <Carousel />
       </div>
       <div className={styles.appointments}>
-        <TransparentBtn className={styles.appointmentBtn}>
-          Setup an Appointment
-        </TransparentBtn>
-        <Link href="/drone" style={{ width: "100%" }}>
+        {/* Icons Row Container */}
+        <div className={styles.iconRowContainer}>
+          <Link
+            href="https://www.icloud.com/sharedalbum/#B2SGY8gBYJRLl6B"
+            target="_blank"
+            // rel="noopener noreferrer"
+            title="View Project Portfolio"
+          >
+            <Image
+              src="/portfolio.png"
+              alt="Portfolio"
+              width={150}
+              height={150}
+              className={styles.iconCircle}
+            />
+          </Link>
+          <Link
+            href="https://www.instagram.com/apexlawncompany?igsh=MXAwYnhiaDI2aWVjaQ=="
+            target="_blank"
+            // rel="noopener noreferrer"
+            title="Visit Instagram"
+          >
+            <Image
+              src="/instagram.png"
+              alt="Instagram"
+              width={150}
+              height={150}
+              className={styles.iconCircle}
+            />
+          </Link>
+        </div>
+
+        {/* Buttons Container */}
+        <div className={styles.buttonsContainer}>
           <TransparentBtn className={styles.appointmentBtn}>
-            Schedule a Drone Fly-by
+            Setup an Appointment
           </TransparentBtn>
-        </Link>
+          <Link href="/drone" style={{ width: "100%" }}>
+            <TransparentBtn className={styles.appointmentBtn}>
+              Schedule a Drone Fly-by
+            </TransparentBtn>
+          </Link>
+        </div>
       </div>
     </div>
   );
